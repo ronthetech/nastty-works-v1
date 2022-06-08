4b3586aa7e752587e94287cb19c305570264ce84
Ronald Jean-Francois <105710107+ronthetech@users.noreply.github.com>
2022-06-07 21:17:38 -0400

    update

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const toggleBtn = document.querySelector('.toggle-menu');
 const links = document.querySelector('.links');
 
 toggleBtn.addEventListener('click', function(){  
-links.classList.toggle('.show-links');
+    links.classList.toggle('.show-links');
 });
